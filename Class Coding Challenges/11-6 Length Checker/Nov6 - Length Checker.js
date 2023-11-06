65a35bb34a4b41d2d95d45a729977b739f71871a
MWBassette <mwbassette@gmail.com>
2023-11-06 17:43:37 +0900

    Changes

--- Class Coding Challenges/11-6 Length Checker/Nov6 - Length Checker.js
+++ Class Coding Challenges/11-6 Length Checker/Nov6 - Length Checker.js
@@ -1,2 +1,5 @@
 var lengthPrompt = prompt("Please type in characters so we can verify the character count:");
 console.log("You have typed " + lengthPrompt.length + " characters. You have " + ('140' - lengthPrompt.length) + " characters left.");
+lengthPrompt.slice(0,140);
+
+// This is also fine:  alert(prompt("Please type in characters so we can verify the character count:").slice(0,140));   
