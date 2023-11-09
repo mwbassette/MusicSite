e1fe63f82c368269da042a6fadfddac03bcd7d27
MWBassette <mwbassette@gmail.com>
2023-11-09 11:34:41 +0900

    Update

---
+++ Class Coding Challenges/7 - Got Milk?.js
@@ -0,0 +1,6 @@
+function milkMoney(money) {
+    var bottles = Math.floor(money / 1.5);
+    console.log("You can buy " + bottles + " bottles of milk if one bottle costs $1.50.");
+}
+
+milkMoney(19);
