4dab9cfef10a93739f73078bb0601638a2d1524a
MWBassette <mwbassette@gmail.com>
2023-12-07 12:10:45 +0900

    Drum Animations

--- Class Coding Challenges/22 - Drum Set/index.js
+++ Class Coding Challenges/22 - Drum Set/index.js
@@ -22,11 +22,13 @@ for (i = 0; i < numberOfDrumButtons; i++) {
         var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     })
 }
 
 document.addEventListener("keydown", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 
 })
 
@@ -69,6 +71,14 @@ function makeSound(key) {
 
         default: console.log(buttonInnerHTML);
     }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100)
 }
 
 
