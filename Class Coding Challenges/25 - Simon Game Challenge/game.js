e655b402612f22ebbdd2b9ff9dbb5443f8b59ae4
MWBassette <mwbassette@gmail.com>
2023-12-15 11:26:04 +0900

    Simon Game Progress1

--- Class Coding Challenges/25 - Simon Game Challenge/game.js
+++ Class Coding Challenges/25 - Simon Game Challenge/game.js
@@ -1,14 +1,28 @@
 // alert("I'm loaded just fine!");
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [randomChosenColor];
-var randomChosenColor = buttonColors[randomNumber];
+
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
 }
 
-if (randomChosenColor = $("button#") {
+var randomChosenColor = buttonColors[randomNumber];
 
+while (randomChosenColor === $("button").attr("id")) {
+    $(this).fadeOut(25).fadeIn(25);
+    var audio = new Audio("sounds/" + this + ".mp3");
+    audio.play();
 }
 
+
+
+
+
+/* TO DO LIST:
 // need to select buttonID that matches randomChosenColor on line 10
+
+
+
+
+*/
