2f508cc27bf020367afc31b713e36469fa27fd36
MWBassette <mwbassette@gmail.com>
2024-03-04 15:08:44 +0900

    Attempt 1 (commented out) to set navbar to transparent upon page refresh before scrolling

--- #6: Site Duplication - TokenTransit/index.js
+++ #6: Site Duplication - TokenTransit/index.js
@@ -3,7 +3,16 @@ window.addEventListener('scroll', setNavbarBackground);
 function setNavbarBackground() {
   const hasUserScrolledDown = document.body.scrollTop > 10 || document.documentElement.scrollTop > 10;
   let navbar = document.getElementById("navbar");
+
   navbar.style.background = hasUserScrolledDown ? "#222" : "transparent";
+
+  // const isUserAtPageTop = document.body.scrollTop < 11 || document.documentElement.scrollTop < 11;
+  // navbar.style.background = isUserAtPageTop ? "transparent" : "#222";
+
+  // if (window.scrollY === 0) {
+  // navbar.style.background = "transparent";
+  // }
+  
 }
 
 
