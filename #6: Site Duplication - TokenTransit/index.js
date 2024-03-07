5237ea173304c158d7073fd0428322e093a22bd7
MWBassette <mwbassette@gmail.com>
2024-03-07 16:20:39 +0900

    Updated class names and added ugly but functional mobile navbar, still needs transitions and active class

--- #6: Site Duplication - TokenTransit/index.js
+++ #6: Site Duplication - TokenTransit/index.js
@@ -11,7 +11,30 @@ function setNavbarBackground() {
     navbar.style.background = hasUserScrolledDown ? "#222" : "transparent";
   }
 }
+
+const mobileNav = document.querySelector('.mobile-nav-button')
+mobileNav.addEventListener('click', displayMobileNavbarLinks);
+
+function displayMobileNavbarLinks() {
+    const links = document.querySelectorAll('.mobile-list-item');
+    links.forEach(li => li.classList.toggle('js-show-mobile-nav-links'));
+
+    
+    // if (links.style.display === "none") {
+    //   links.style.display = "block";
+    // } else {
+    //   links.style.display = "none";
+    // }
+}
   
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// } 
 
 
 
