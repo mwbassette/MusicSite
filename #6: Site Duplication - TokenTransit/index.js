508465d037a56052ef77f7af17b11e1e0a5d7f66
MWBassette <mwbassette@gmail.com>
2024-03-07 10:57:04 +0900

    Improved navbar responsiveness & consistency to handle resizing, refreshing, and 3 device types

--- #6: Site Duplication - TokenTransit/index.js
+++ #6: Site Duplication - TokenTransit/index.js
@@ -1,20 +1,26 @@
 window.addEventListener('scroll', setNavbarBackground);
+window.addEventListener('resize', setNavbarBackground);
 
 function setNavbarBackground() {
   const hasUserScrolledDown = document.body.scrollTop > 10 || document.documentElement.scrollTop > 10;
   let navbar = document.getElementById("navbar");
 
-  navbar.style.background = hasUserScrolledDown ? "#222" : "transparent";
+  if (window.screen.width <= 767) {
+    navbar.style.background = "#222";
+  } else {
+    navbar.style.background = hasUserScrolledDown ? "#222" : "transparent";
+  }
+}
 
-  // const isUserAtPageTop = document.body.scrollTop < 11 || document.documentElement.scrollTop < 11;
+
+
+ // const isUserAtPageTop = document.body.scrollTop < 11 || document.documentElement.scrollTop < 11;
   // navbar.style.background = isUserAtPageTop ? "transparent" : "#222";
 
   // if (window.scrollY === 0) {
   // navbar.style.background = "transparent";
   // }
   
-}
-
 
 
 
