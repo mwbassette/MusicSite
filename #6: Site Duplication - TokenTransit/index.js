83dac8a06d5824e1bc248e5c77b113915989769b
MWBassette <mwbassette@gmail.com>
2024-03-07 10:57:20 +0900

    Cleared out js comment

--- #6: Site Duplication - TokenTransit/index.js
+++ #6: Site Duplication - TokenTransit/index.js
@@ -11,15 +11,6 @@ function setNavbarBackground() {
     navbar.style.background = hasUserScrolledDown ? "#222" : "transparent";
   }
 }
-
-
-
- // const isUserAtPageTop = document.body.scrollTop < 11 || document.documentElement.scrollTop < 11;
-  // navbar.style.background = isUserAtPageTop ? "transparent" : "#222";
-
-  // if (window.scrollY === 0) {
-  // navbar.style.background = "transparent";
-  // }
   
 
 
