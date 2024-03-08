d8fa3cbbbe9dc102134b377d785c7ae135c2aa8c
MWBassette <mwbassette@gmail.com>
2024-03-08 09:33:55 +0900

    Cleaned up js comments of previous function ideas, and cleaned up css a bit for ordering and removing unnecessary classes

--- #6: Site Duplication - TokenTransit/index.js
+++ #6: Site Duplication - TokenTransit/index.js
@@ -18,23 +18,8 @@ mobileNav.addEventListener('click', displayMobileNavbarLinks);
 function displayMobileNavbarLinks() {
     const links = document.querySelectorAll('.mobile-list-item');
     links.forEach(li => li.classList.toggle('js-show-mobile-nav-links'));
-
-    
-    // if (links.style.display === "none") {
-    //   links.style.display = "block";
-    // } else {
-    //   links.style.display = "none";
-    // }
 }
   
-// function myFunction() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// } 
 
 
 
