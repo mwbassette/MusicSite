ed8ea0c87124ee80261243b77bb354ec5799be45
MWBassette <mwbassette@gmail.com>
2024-03-08 12:19:56 +0900

    Started function for responsive navbar

--- #7: Painting Image Gallery Website/index.js
+++ #7: Painting Image Gallery Website/index.js
@@ -6,3 +6,19 @@ function showBoxArt() {
     boxArtContainer.classList.toggle('js-expand');
     boxArtContainer.classList.toggle('js-hide');
 }
+
+
+// const mobileNavButton = document.querySelector(".mobile-only-nav-button");
+// mobileNavButton.addEventListener('click', showNavLinks);
+
+// function showNavLinks() {
+    
+//     const navLinks = document.querySelectorAll(".list-item > a");
+    
+//     document.querySelector(".nav-link-list-container").classList.toggle("hide-on-mobile");
+//     mobileNavButton.classList.toggle("mobile-position");
+
+    
+//     navLinks.classList.toggle("expand-for-mobile");
+// }
+    
