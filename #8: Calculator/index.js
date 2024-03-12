ae9a17a03cbee47d607b29545bdf92109331e254
MWBassette <mwbassette@gmail.com>
2024-03-12 11:33:33 +0900

    Found a way to append multiple numbers to display

--- #8: Calculator/index.js
+++ #8: Calculator/index.js
@@ -3,27 +3,35 @@ const operatorButtons = document.querySelectorAll(".operator");
 const clearButton = document.querySelector(".clear");
 const equalButton = document.querySelector(".equal");
 const display = document.querySelector(".display-area");
-const num1 = [];
-const num2 = "";
+
+let currentNumber = '';
+let previousNumber = '';
 
 
 
 
 clearButton.addEventListener('click', clearDisplay);
 equalButton.addEventListener('click', calculate);
-numberButtons.addEventListener('click', updateDisplay);
-// numberButtons.forEach((number) => {
-//     display.textContent = number
-// });
+
+
+numberButtons.forEach(numBtn => {
+    numBtn.addEventListener('click', () => {
+        appendNumber(numBtn.innerText);
+        // updateDisplay();
+    });
+  });
+
+function appendNumber(newNum) {
+    currentNumber = currentNumber.toString() + newNum.toString();
+    display.textContent = currentNumber;
+}
+
 
 function clearDisplay() {
     display.textContent = "";
 }
 function updateDisplay() {
-    numberButtons.forEach((number) => {
-        num1.push(number);
-        display.textContent = num1;
-    })
+
 }
 function calculate() {
 // return evaluation of: (num1 operator num2)
