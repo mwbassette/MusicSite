671f551d81893625c70aecc0c754a8c4e8f1ffcb
MWBassette <mwbassette@gmail.com>
2024-03-12 14:08:30 +0900

    Working calculator, removed contents and updated innerText properties

--- #8: Calculator/index.js
+++ #8: Calculator/index.js
@@ -6,44 +6,75 @@ const display = document.querySelector(".display-area");
 
 let currentNumber = '';
 let previousNumber = '';
-
-
-
-
-clearButton.addEventListener('click', clearDisplay);
-equalButton.addEventListener('click', calculate);
-
+let operation = null;
 
 numberButtons.forEach(numBtn => {
     numBtn.addEventListener('click', () => {
         appendNumber(numBtn.innerText);
-        // updateDisplay();
     });
   });
+operatorButtons.forEach(oprBtn => {
+    oprBtn.addEventListener('click', () => {
+        handleSelectedOperation(oprBtn.innerText);
+    })
+})
 
+clearButton.addEventListener('click', clearDisplay);
+equalButton.addEventListener('click', calculate);
+
+function appendNumber(newNum) {
+    currentNumber = currentNumber.toString() + newNum.toString();
+    display.innerText = currentNumber;
+    limitNumberCount();
+}
+function handleSelectedOperation(selectedOperation) {
+    if (currentNumber === '') return;
+    if (previousNumber !== '') {
+        calculate();
+    }
+    operation = selectedOperation;
+    previousNumber = currentNumber;
+    currentNumber = '';
+}
+function calculate() {
+    let calculation;
+    const prev = parseFloat(previousNumber);
+    const cur = parseFloat(currentNumber);
+    if (isNaN(prev) || isNaN(cur)) return;
+    switch (operation) {
+        case '÷':
+            calculation = prev / cur;
+            break;
+        case '×':
+            calculation = prev * cur;
+            break;
+        case '−':
+            calculation = prev - cur;
+            break;
+        case '+':
+            calculation = prev + cur;
+            break;
+        default:
+            return;
+    }
+    currentNumber = calculation;
+    operation = undefined;
+    previousNumber = '';
+    updateDisplay();
+}    
+function clearDisplay() {
+    display.innerText = '';
+    currentNumber = '';
+    previousNumber = '';
+}
+function updateDisplay() {
+    display.innerText = currentNumber;
+}
 function limitNumberCount() {
     if (currentNumber.length > 13) {
         currentNumber = currentNumber.substring(0,13);
     }
 }
-
-function appendNumber(newNum) {
-    currentNumber = currentNumber.toString() + newNum.toString();
-    display.textContent = currentNumber;
-    limitNumberCount();
-}
-
-
-function clearDisplay() {
-    display.textContent = '';
-    currentNumber = '';
-    previousNumber = '';
-}
-function updateDisplay() {
-
-}
-function calculate() {
-// return evaluation of: (num1 operator num2)
 }
 
 
