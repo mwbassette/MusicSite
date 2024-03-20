204bd2eaa7f6c16bd2fef204a461a04448a72758
MWBassette <mwbassette@gmail.com>
2024-03-20 16:30:37 +0900

    Added mobile navbar functionality and updated css

--- #7: Painting Image Gallery Website/index.js
+++ #7: Painting Image Gallery Website/index.js
@@ -1,18 +1,23 @@
 //navbar functionality
+const navLinkContainer = document.querySelector(".nav-link-list-container")
+const navLinkList = document.querySelector(".nav-list")
+const navLinks = document.querySelectorAll(".list-item > a")
+const mobileNavButton = document.querySelector(".mobile-only-nav-button")
+
 document.addEventListener('click', handleClickForMobileNavBar)
 
 function handleClickForMobileNavBar() {
-    const mobileNavButton = document.querySelector(".mobile-only-nav-button")
     mobileNavButton.addEventListener('click', showNavLinks)
 }
 function showNavLinks() {
-    const navLinks = document.querySelectorAll(".list-item > a")
-    const navLinkContainer = document.querySelector(".nav-link-list-container")
-
-    navLinkContainer.classList.toggle("hide-on-mobile")
     mobileNavButton.classList.toggle("mobile-position")
-    navLinks.classList.toggle("expand-for-mobile")
+    navLinkContainer.classList.toggle("hide-on-mobile")
+    navLinkList.classList.toggle("expand-for-mobile")
+    navLinks.forEach(link => {
+        link.classList.toggle('expand-for-mobile')
+    })
 }
+
 //boxart functionality
 document.addEventListener('click', handleClickForBoxArtLink())
 
