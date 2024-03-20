c6fe3d17cc8e31e2ac1f84db882937e17ef75dbd
MWBassette <mwbassette@gmail.com>
2024-03-20 10:37:13 +0900

    Updated function scoping for box art handlers, navbar in progress

--- #7: Painting Image Gallery Website/index.js
+++ #7: Painting Image Gallery Website/index.js
@@ -1,24 +1,30 @@
-const boxArtLink = document.querySelector('#div-box-art-link');
-boxArtLink.addEventListener('click', showBoxArt);
+//navbar functionality
+document.addEventListener('click', handleClickForMobileNavBar)
 
+function handleClickForMobileNavBar() {
+    const mobileNavButton = document.querySelector(".mobile-only-nav-button")
+    mobileNavButton.addEventListener('click', showNavLinks)
+}
+function showNavLinks() {
+    const navLinks = document.querySelectorAll(".list-item > a")
+    const navLinkContainer = document.querySelector(".nav-link-list-container")
+
+    navLinkContainer.classList.toggle("hide-on-mobile")
+    mobileNavButton.classList.toggle("mobile-position")
+    navLinks.classList.toggle("expand-for-mobile")
+}
+//boxart functionality
+document.addEventListener('click', handleClickForBoxArtLink())
+
+function handleClickForBoxArtLink() {
+    const boxArtLink = document.querySelector('#div-box-art-link')
+    boxArtLink.addEventListener('click', showBoxArt)
+}
 function showBoxArt() {
-    const boxArtContainer = document.querySelector('.div-box-art-container');
-    boxArtContainer.classList.toggle('js-expand');
-    boxArtContainer.classList.toggle('js-hide');
+    const boxArtContainer = document.querySelector('.div-box-art-container')
+    boxArtContainer.classList.toggle('js-expand')
+    boxArtContainer.classList.toggle('js-hide')
 }
 
 
-// const mobileNavButton = document.querySelector(".mobile-only-nav-button");
-// mobileNavButton.addEventListener('click', showNavLinks);
 
-// function showNavLinks() {
-    
-//     const navLinks = document.querySelectorAll(".list-item > a");
-    
-//     document.querySelector(".nav-link-list-container").classList.toggle("hide-on-mobile");
-//     mobileNavButton.classList.toggle("mobile-position");
-
-    
-//     navLinks.classList.toggle("expand-for-mobile");
-// }
-    
