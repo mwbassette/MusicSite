c80f25883465b35c57c4c4202c4f4f8894a00f3c
MWBassette <mwbassette@gmail.com>
2024-03-21 15:00:29 +0900

    Established nav button functionality & expected color behavior, still doesn't behave in dev tools

--- #6: Site Duplication - TokenTransit/index.js
+++ #6: Site Duplication - TokenTransit/index.js
@@ -1,15 +1,15 @@
 //Nav Background Functionality:
-window.addEventListener('scroll', setNavbarBackground);
-window.addEventListener('resize', setNavbarBackground);
-setNavbarBackground();
+window.addEventListener('scroll', setNavbarBackground)
+window.addEventListener('resize', setNavbarBackground)
+setNavbarBackground()
 
 function setNavbarBackground() {
   const mobileWindowSize = window.innerWidth <= 767
   const hasUserScrolledDown = document.body.scrollTop > 10 || document.documentElement.scrollTop > 10;
-  let navbar = document.getElementById("navbar");
-  if (!navbar) return;
+  let navbar = document.getElementById("navbar")
+  if (!navbar) return
   if (!mobileWindowSize) {
-    navbar.style.background = hasUserScrolledDown ? "#222" : "transparent";
+    navbar.style.background = hasUserScrolledDown ? "#222" : "transparent"
   } else if (mobileWindowSize) {
     navbar.style.background = "#222"
   }
@@ -17,15 +17,18 @@ function setNavbarBackground() {
 
 //Nav Button Functionality:
 const mobileNav = document.querySelector('.mobile-nav-button')
+mobileNav.addEventListener('click', changeMobileButtonBackgroundColor)
 mobileNav.addEventListener('click', displayMobileNavbarLinks);
-mobileNav.addEventListener('click', changeMobileButtonBackgroundColor);
 
-function displayMobileNavbarLinks() {
-    const links = document.querySelectorAll('.mobile-list-item');
-    links.forEach(li => li.classList.toggle('js-show-mobile-nav-links'));
-}
 function changeMobileButtonBackgroundColor() {
-      mobileNav.classList.toggle('active-button');    
+  if (!mobileNav) return
+  mobileNav.classList.toggle('button-is-focused')
 }
+function displayMobileNavbarLinks() {
+  if (!mobileNav) return
+  const links = document.querySelectorAll('.mobile-list-item')
+  links.forEach(li => li.classList.toggle('js-show-mobile-nav-links'))
+}
+
 
 
