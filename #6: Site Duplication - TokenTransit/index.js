29047179a3099f60871e0e413386e359943e02eb
MWBassette <mwbassette@gmail.com>
2024-03-21 13:50:08 +0900

    Removed unused css class, started working on :hover, and finally fixed nav background logic for consistent display across browsers

--- #6: Site Duplication - TokenTransit/index.js
+++ #6: Site Duplication - TokenTransit/index.js
@@ -1,17 +1,21 @@
+//Nav Background Functionality:
 window.addEventListener('scroll', setNavbarBackground);
 window.addEventListener('resize', setNavbarBackground);
+setNavbarBackground();
 
 function setNavbarBackground() {
+  const mobileWindowSize = window.innerWidth <= 767
   const hasUserScrolledDown = document.body.scrollTop > 10 || document.documentElement.scrollTop > 10;
   let navbar = document.getElementById("navbar");
   if (!navbar) return;
-  if (window.screen.width <= 767) {
-    navbar.style.background = "#222";
-  } else {
+  if (!mobileWindowSize) {
     navbar.style.background = hasUserScrolledDown ? "#222" : "transparent";
+  } else if (mobileWindowSize) {
+    navbar.style.background = "#222"
   }
 }
 
+//Nav Button Functionality:
 const mobileNav = document.querySelector('.mobile-nav-button')
 mobileNav.addEventListener('click', displayMobileNavbarLinks);
 mobileNav.addEventListener('click', changeMobileButtonBackgroundColor);
