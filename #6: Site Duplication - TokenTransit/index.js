7edc7c1f037d224f99ab7660aaef833f7c0cf258
MWBassette <mwbassette@gmail.com>
2024-03-22 14:15:08 +0900

    Updated JS as per conversation

--- #6: Site Duplication - TokenTransit/index.js
+++ #6: Site Duplication - TokenTransit/index.js
@@ -6,27 +6,33 @@ setNavbarBackground()
 function setNavbarBackground() {
   const mobileWindowSize = window.innerWidth <= 767
   const hasUserScrolledDown = document.body.scrollTop > 10 || document.documentElement.scrollTop > 10;
-  let navbar = document.getElementById("navbar")
+  const navbar = document.getElementById("navbar")
   if (!navbar) return
   if (!mobileWindowSize) {
     navbar.style.background = hasUserScrolledDown ? "#222" : "transparent"
-  } else if (mobileWindowSize) {
+  } else {
     navbar.style.background = "#222"
   }
 }
 
 //Nav Button Functionality:
 const mobileNav = document.querySelector('.mobile-nav-button')
-mobileNav.addEventListener('click', changeMobileButtonBackgroundColor)
-mobileNav.addEventListener('click', displayMobileNavbarLinks);
+addMobileNavClickHandling()
 
+function addMobileNavClickHandling() {
+  if (!mobileNav) return
+  mobileNav.addEventListener('click', handleMobileNavClick);
+}
+function handleMobileNavClick() {
+  changeMobileButtonBackgroundColor()
+  displayMobileNavbarLinks()
+}
 function changeMobileButtonBackgroundColor() {
   if (!mobileNav) return
   mobileNav.classList.toggle('button-is-focused')
 }
 function displayMobileNavbarLinks() {
   const links = document.querySelectorAll('.mobile-list-item')
-  if (!links) return
   links.forEach(li => li.classList.toggle('js-show-mobile-nav-links'))
 }
 
